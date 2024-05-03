3922649f7c7aafdeb362b5086fb0148a469e398b
Gustavo Domenech <gustavo.domenechs@gmail.com>
2024-05-03 15:51:10 -0300

    Update 13 . Roman to Integer.c

--- 13 . Roman to Integer.c
+++ 13 . Roman to Integer.c
@@ -3,37 +3,37 @@ int romanToInt(char* s) {
     for(i=0;s[i];i++){
       switch (s[i]){
         case 'I':
-			    if(s[i+1]=='V' || s[i+1]=='X'){
-				    sum-=1;
-			    }
-			    else
-			      sum+=1;
+		if(s[i+1]=='V' || s[i+1]=='X'){
+			sum-=1;
+		}
+		else
+			sum+=1;
         break;
         case 'V':
-			      sum+=5;
+			sum+=5;
         break;
-		    case 'X':
-			    if(s[i+1]=='L' || s[i+1]=='C'){
-				    sum-=10;
-			    }
-			    else
-			      sum+=10;
+	case 'X':
+		if(s[i+1]=='L' || s[i+1]=='C'){
+			sum-=10;
+		}
+		else
+			sum+=10;
         break;
-		    case 'L':
-		        sum+=50;
+	case 'L':
+			sum+=50;
         break;
-		    case 'C':
-			    if(s[i+1]=='D' || s[i+1]=='M'){
-			      sum-=100;
-			    }
-			    else
-			      sum+=100;
+	case 'C':
+		if(s[i+1]=='D' || s[i+1]=='M'){
+			sum-=100;
+		}
+		else
+			sum+=100;
         break;
-		    case 'D':
-		        sum+=500;
+	      case 'D':
+		      sum+=500;
         break;
-		    case 'M':
-		        sum+=1000;
+	      case 'M':
+		      sum+=1000;
         break;
         }
     }
